a283b59f085d9b3f569834287519ddaed68c4759
Aleksandr Okhotnycov <sashaohotni26@gmail.com>
2021-01-27 17:22:18 +0200

    32

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "./dist"),
-        publicPath: "/ciklum-tech-task/",
+        publicPath: "/dist/",
     },
     module: {
         rules: [{
